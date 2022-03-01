ad81f247ee78ea0ee8b8fe1b8294bc5bb094706a
Boramy <boramy.khuon@gmail.com>
2022-03-01 09:24:04 +0100

    ajout card screen categorie

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,22 +1,141 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView} from 'react-native';
+import {Card} from 'react-native-elements';
+
+
+
 
 
 const CategoriesScreen = () => {
     return (
-<View style={styles.container}>
-<Text>CategoriesScreen</Text>
-</View>
+      <>
+      <ScrollView style={styles.body}>
+      
+        <View style={styles.body}>
+        <View style={styles.title}>
+          <Text style={styles.bigtitle}>La Fraiche</Text>
+          <Text style={styles.subtitle}>Catégories</Text>
+        </View>
+
+        <View style={styles.container}>
+          <Card containerStyle={styles.category}>
+             <Card.Image
+              style={styles.imageup}
+              source={require('../assets/cat-fruits.png')}
+            />
+            <Text style={styles.categoryname}>Fruits</Text>
+          </Card>
+
+          <Card containerStyle={styles.category}>
+             <Card.Image
+              style={styles.imageup}
+              source={require('../assets/cat-légumes.png')}
+            />
+            <Text style={styles.categoryname}>Légumes</Text>
+          </Card>
+
+          <Card containerStyle={styles.category}>
+             <Card.Image
+              style={styles.imagedown}
+              source={require('../assets/cat-panier-oeufs.png')}
+            />
+            <Text style={styles.categoryname}>Oeufs</Text>
+          </Card>
+
+          <Card containerStyle={styles.category}>
+             <Card.Image
+              style={styles.imagedown}
+              source={require('../assets/cat-laitage.png')}
+            />
+            <Text style={styles.categoryname}>Laitages</Text>
+          </Card>
+          </View>
+        </View>
+        </ScrollView>
+      </>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+  body: {
+    backgroundColor: '#ffffff',
+    // paddingLeft: '80',
+    // paddingRight: '80',
+    // paddingTop: 147,
+    // paddingBottom: '70%',
+  },
+  
+  title: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    marginTop: 147,
+  },
+
+  bigtitle: {
+    // fontFamily: "Rochester",
+
+    color:'#53B175',
+  },
+
+  subtitle: {
+    // fontFamily: "Montserrat",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 100,
+    marginBottom: 20,
+  },
+  
+  container: {
       flex: 1,
-      backgroundColor: '#ffffff',
       alignItems: 'center',
       justifyContent: 'center',
+      display: "flex",
+      flexDirection: "row",
+      flexWrap : "wrap",
+      
     },
+
+    category : {
+      width: 164,
+      height: 190,
+      borderRadius : 18,
+      backgroundColor: '#53B175',
+      borderStyle: 'solid',
+      borderColor: '#5375752B',
+
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 8,
+      },
+      shadowOpacity: 0.44,
+      shadowRadius: 10.32,
+
+      elevation: 16,
+    },
+
+    imageup: {
+      width: 130,
+      height: 80,
+      marginBottom: 10,
+      marginTop: 30,
+    },
+
+    imagedown: {
+      width: 130,
+      height: 130,
+      marginBottom: 10
+    },
+
+    categoryname : {
+      textAlign : 'center',
+      color: '#ffffff',
+      fontFamily: 'Montserrat',
+      fontWeight: 'bold',
+
+    }
   });    
     
 export default CategoriesScreen;
--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+//Mettre les 2 lignes de script dessous juste avant la fermeture du body pour utiliser l'icone
+// <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+//<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
 
 const ProductScreen = () => {
     return (
-<View style={styles.container}>
-<Text>ProductScreen</Text>
-</View>
+      <>
+      <ScrollView>
+        <View style={styles.body}>
+            <View style={styles.bigtitle}></View>
+            
+              <View style={styles.container}>
+                  <Card containerStyle={styles.product}>
+                  {/* <ion-icon name="heart-outline"></ion-icon> */}
+                  <Card.Image
+                      style={styles.image}
+                      source={require('../assets/cat-fruits.png')}
+                      />
+                  <View style={styles.textdetails}>
+                    <Text>Titre</Text>
+                    <Text>Prix €</Text>
+                  </View>
+                  <Text>quantite</Text>
+
+                  <Button title='détails' />
+
+                  </Card>
+              </View>
+        </View>
+
+        
+      </ScrollView>
+      </>
+
     );
 };
 
