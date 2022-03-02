cabcd1f0838217c6c99a04b4185182a8a96db672
Elodie <elodie@MacBook-Pro-de-Elodie.local>
2022-03-02 12:59:52 +0100

    resolution des conflits git

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import DropShadow from "react-native-drop-shadow";
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from "@expo/vector-icons";
 
 import HomeScreen from "./screens/HomeScreen";
-import AccountScreen from './screens/AccountScreen';
-import FavouriteScreen from './screens/FavouriteScreen';
-import BasketScreen from './screens/BasketScreen';
-import CategoriesScreen from './screens/CategoriesScreen';
-import LogInScreen from './screens/LogInScreen';
+import AccountScreen from "./screens/AccountScreen";
+import FavouriteScreen from "./screens/FavouriteScreen";
+import BasketScreen from "./screens/BasketScreen";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import LogInScreen from "./screens/LogInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
-import LockerScreen from './screens/LockerScreen';
-import ProductScreen from './screens/ProductScreen';
-import DetailScreen from './screens/DetailScreen';
-import LockerScreen from './screens/LockerScreen';
+import LockerScreen from "./screens/LockerScreen";
+import ProductScreen from "./screens/ProductScreen";
+import DetailScreen from "./screens/DetailScreen";
+
+import SuccessScreen from "./screens/SuccessScreen";
+import ErrorScreen from "./screens/ErrorScreen";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -31,29 +32,28 @@ const BottomNavigator = () => {
         tabBarIcon: ({ color }) => {
           let iconName;
 
-          if (route.name == 'Categories') {
-            iconName = 'home';
-          } else if (route.name == 'Account') {
-            iconName = 'user';
-          }else if (route.name == 'Favourite') {
-            iconName = 'heart';
-          }else if (route.name == 'Basket') {
-            iconName = 'shopping-basket';
+          if (route.name == "Categories") {
+            iconName = "home";
+          } else if (route.name == "Account") {
+            iconName = "user";
+          } else if (route.name == "Favourite") {
+            iconName = "heart";
+          } else if (route.name == "Basket") {
+            iconName = "shopping-basket";
           }
           return <FontAwesome name={iconName} size={24} color={color} />;
         },
-        })}
-
+      })}
       tabBarOptions={{
         showLabel: false,
-        activeTintColor: '#404040',
-        inactiveTintColor: '#A6A6A6',
+        activeTintColor: "#404040",
+        inactiveTintColor: "#A6A6A6",
         style: {
-          backgroundColor: '#D9D9D9',
-        marginTop:20,
-        borderTopLeftRadius: 10, 
-        borderTopRightRadius: 10,
-        }
+          backgroundColor: "#D9D9D9",
+          marginTop: 20,
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
+        },
       }}
     >
       <Tab.Screen name="Categories" component={CategoriesScreen} />
@@ -62,23 +62,23 @@ const BottomNavigator = () => {
       <Tab.Screen name="Basket" component={BasketScreen} />
     </Tab.Navigator>
   );
-}
+};
 
 export default function App() {
   return (
-
-<NavigationContainer>
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="SignIn" component={LogInScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="Locker" component={LockerScreen} />
-      <Stack.Screen name="BottomNavigator" component={BottomNavigator}/>
-      <Stack.Screen name="Product" component={ProductScreen} />
-      <Stack.Screen name="Detail" component={DetailScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SignIn" component={LogInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Locker" component={LockerScreen} />
+        <Stack.Screen name="Success" component={SuccessScreen} />
+        <Stack.Screen name="Account" component={AccountScreen} />
+        <Stack.Screen name="Error" component={ErrorScreen} />
+        <Stack.Screen name="BottomNavigator" component={BottomNavigator} />
+        <Stack.Screen name="Product" component={ProductScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
--- screens/LockerScreen.js
+++ screens/LockerScreen.js
@@ -1,20 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView  } from 'react-native';
-import { Overlay, Input, CheckBox, Button} from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useState, useEffect } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Overlay, Input, CheckBox, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-
-<<<<<<< HEAD
-export default function LockerScreen() {
-=======
-export default function Lockers() {
->>>>>>> lockersAB
-
-  const [alignSelf, setAlignSelf] = useState("flex-end");
+export default function LockerScreen(props) {
   const [ferme1, setFerme1] = useState(false);
   const [ferme2, setFerme2] = useState(false);
   const [ferme3, setFerme3] = useState(false);
@@ -29,50 +22,29 @@ export default function Lockers() {
   const [listPOI, setListPOI] = useState([]);
   const [isVisible, setIsVisible] = useState(false);
 
-  //const [titrePOI, setTitrePOI] = useState();
-  //const [descPOI, setDescPOI] = useState();
-
-  const [tempPOI, setTempPOI] = useState();
-
   useEffect(() => {
     async function askPermissions() {
       let { status } = await Permissions.askAsync(Permissions.LOCATION);
-      if (status === 'granted') {
-        Location.watchPositionAsync({ distanceInterval: 2 },
-          (location) => {
-            setCurrentLatitude(location.coords.latitude)
-            setCurrentLongitude(location.coords.longitude);
-          }
-        );
+      if (status === "granted") {
+        Location.watchPositionAsync({ distanceInterval: 2 }, (location) => {
+          setCurrentLatitude(location.coords.latitude);
+          setCurrentLongitude(location.coords.longitude);
+        });
       }
     }
     askPermissions();
   }, []);
 
-  
-
-  /*var selectPOI = (e) => {
-    if (addPOI) {
-      setAddPOI(false);
-      setIsVisible(true);
-      setTempPOI({ latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude });
-    }
-  }*/
-
-  /*var handleSubmit = () => {
-    setListPOI([...listPOI, { longitude: tempPOI.longitude, latitude: tempPOI.latitude, titre: titrePOI, description: descPOI }]);
-    setIsVisible(false);
-    setTempPOI();
-    setDescPOI();
-    setTitrePOI();
-  }*/
-
-
   var markerPOI = listPOI.map((POI, i) => {
-    return <Marker key={i} pinColor="blue" coordinate={{ latitude: POI.latitude, longitude: POI.longitude }}
-      title={POI.titre}
-      description={POI.description}
-    />
+    return (
+      <Marker
+        key={i}
+        pinColor="blue"
+        coordinate={{ latitude: POI.latitude, longitude: POI.longitude }}
+        title={POI.titre}
+        description={POI.description}
+      />
+    );
   });
   var isDisabled = false;
   if (addPOI) {
@@ -80,72 +52,97 @@ export default function Lockers() {
   }
 
   return (
-    <View style={{ flex: 1 }} >
-
-            <View style = {{flexDirection: 'row', margin: 10}}>
-              <Text  style={{flex: 1, fontSize:20, }}>
-                Votre panier : 
-              </Text>
-              <Text style={{textAlign: 'right', flex: 1, fontSize:20}}>100€</Text>
-            </View>
-            
-       <View>   
-      <Overlay overlayStyle = {{width: '70%', height:'70%', borderRadius:'10%', padding:-10}}
-        isVisible={isVisible}
-        onBackdropPress={() => { setIsVisible(false) }} 
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          alignItems: "center",
+          textAlign: "center",
+          paddingTop: 40,
+          backgroundColor: "#53B175",
+          paddingBottom: 15,
+        }}
       >
-        
-      <ScrollView>
-        <View style={{ flex: 1, alignItems: 'center',
-         justifyContent: 'center',
-         backgroundColor:'#53B175', borderTopLeftRadius:'10px', borderTopRightRadius:'10px'}}>
-          <Text style={{ color: 'white', fontSize:25, margin:'5%' }}>Je choisis mon locker</Text>
-          </View>  
-          
-            <CheckBox     
-          title=" La ferme des 4 étoiles montante du dev - 2 rue des retrouvailles, 75020 Paris" 
-          checked={ferme1}
-              onPress={() => setFerme1(!ferme1)} 
-        />
-        <CheckBox
-          title="La ferme de la Couturière Elodie - 84 bd de la ferme, 75011 Paris" 
-          checked={ferme2}
-              onPress={() => setFerme2(!ferme2)} 
-        />
-        <CheckBox
-          title="La ferme de Renaud - 32 rue de la paix, 75009 Paris" 
-          checked={ferme3}
-              onPress={() => setFerme3(!ferme3)}  
-        />
-        <CheckBox
-          title="La Ferme ouverte de Boramy - 16 bd de courcelle, 75017 Paris"
-          checked={ferme4}
-              onPress={() => setFerme4(!ferme4)}
-        />
-        <CheckBox
-          title="La ferme du Vilain Amour - 2 rue de paris 75005 Paris"
-          checked={ferme5}
-              onPress={() => setFerme5(!ferme5)}
-        />
-        
-        
-        </ScrollView>
-        
-          <Button
-            title="Confirmer"
-            buttonStyle={{ backgroundColor: '#53B175', borderRadius: '10px' }}
-                containerStyle={{
-                  marginHorizontal: 70,
-                  marginVertical: 10,
-                  alignItems: 'center', 
-                  justifyContent: 'center'}}
-          />
-        
-      </Overlay>
+        <Text style={{ color: "white", fontSize: 18 }}>Mon casier</Text>
       </View>
 
-      <MapView 
-        onPress={() => { setIsVisible(!isVisible) }}
+      <View style={{ flexDirection: "row", margin: 10 }}>
+        <Text style={{ flex: 1, fontSize: 20 }}>Votre panier :</Text>
+        <Text style={{ textAlign: "right", flex: 1, fontSize: 20 }}>100€</Text>
+      </View>
+
+      <View>
+        <Overlay
+          overlayStyle={{
+            height: "60%",
+            width: "80%",
+            borderRadius: 10,
+            padding: -10,
+          }}
+          isVisible={isVisible}
+          onBackdropPress={() => {
+            setIsVisible(false);
+          }}
+        >
+          <ScrollView>
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#53B175",
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 25, margin: 5 }}>
+                Je choisis mon locker
+              </Text>
+            </View>
+
+            <CheckBox
+              title=" La ferme des 4 étoiles montante du dev - 2 rue des retrouvailles, 75020 Paris"
+              checked={ferme1}
+              onPress={() => setFerme1(!ferme1)}
+            />
+            <CheckBox
+              title="La ferme de la Couturière Elodie - 84 bd de la ferme, 75011 Paris"
+              checked={ferme2}
+              onPress={() => setFerme2(!ferme2)}
+            />
+            <CheckBox
+              title="La ferme de Renaud - 32 rue de la paix, 75009 Paris"
+              checked={ferme3}
+              onPress={() => setFerme3(!ferme3)}
+            />
+            <CheckBox
+              title="La Ferme ouverte de Boramy - 16 bd de courcelle, 75017 Paris"
+              checked={ferme4}
+              onPress={() => setFerme4(!ferme4)}
+            />
+            <CheckBox
+              title="La ferme du Vilain Amour - 2 rue de paris 75005 Paris"
+              checked={ferme5}
+              onPress={() => setFerme5(!ferme5)}
+            />
+          </ScrollView>
+
+          <Button
+            title="Confirmer"
+            buttonStyle={{ backgroundColor: "#53B175", borderRadius: 10 }}
+            containerStyle={{
+              marginHorizontal: 70,
+              marginVertical: 10,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          />
+        </Overlay>
+      </View>
+
+      <MapView
+        onPress={() => {
+          setIsVisible(!isVisible);
+        }}
         style={{ flex: 1 }}
         initialRegion={{
           latitude: 48.866667,
@@ -154,83 +151,97 @@ export default function Lockers() {
           longitudeDelta: 0.0421,
         }}
       >
-
-
-        <Marker key={"currentPos"}
+        <Marker
+          key={"currentPos"}
           pinColor="red"
           title="Hello"
           description="I'am here"
-          coordinate={{ latitude: currentLatitude, longitude: currentLongitude }}
+          coordinate={{
+            latitude: currentLatitude,
+            longitude: currentLongitude,
+          }}
         />
-        <Marker key={"currentPos1"}
+        <Marker
+          key={"currentPos1"}
           pinColor="blue"
           title=" La ferme des 4 étoiles montante du dev"
           description="part ici houhouuu, choisit nous stp"
-          coordinate={{ latitude: 48.872939393965694, longitude: 2.2701871445515867 }}
+          coordinate={{
+            latitude: 48.872939393965694,
+            longitude: 2.2701871445515867,
+          }}
         />
-        <Marker key={"currentPos2"}
+        <Marker
+          key={"currentPos2"}
           pinColor="blue"
           title="La ferme de la Couturière Elodie"
           description="Elodie notre fermière t'accueil avec des radis en bienvenue"
-          coordinate={{ latitude: 48.88817952018065, longitude: 2.330182795535716 }}
+          coordinate={{
+            latitude: 48.88817952018065,
+            longitude: 2.330182795535716,
+          }}
         />
-        <Marker key={"currentPos3"}
+        <Marker
+          key={"currentPos3"}
           pinColor="blue"
           title="La ferme de Renaud"
           description="Renaud le prénom pas la marque de voiture hein !!!"
-          coordinate={{ latitude: 48.846340539193704, longitude: 2.335862530428087 }}
+          coordinate={{
+            latitude: 48.846340539193704,
+            longitude: 2.335862530428087,
+          }}
         />
-        <Marker key={"currentPos4"}
+        <Marker
+          key={"currentPos4"}
           pinColor="blue"
           title="La Ferme ouverte de Boramy"
-          description="C'est la ferme qui est ouverte, pas moi te méprend pas !"
-          coordinate={{ latitude: 48.82914374967133, longitude: 2.431566206320608 }}
+          description="Je ne suis pas fermier, pas moi te méprend pas !"
+          coordinate={{
+            latitude: 48.82914374967133,
+            longitude: 2.431566206320608,
+          }}
         />
-        <Marker key={"currentPos5"}
+        <Marker
+          key={"currentPos5"}
           pinColor="blue"
           title="La ferme du Vilain Amour"
           description="choisit moi ou je vais te retrouver ... "
-          coordinate={{ latitude: 48.904704768930785, longitude: 2.4037320810788603 }}
+          coordinate={{
+            latitude: 48.904704768930785,
+            longitude: 2.4037320810788603,
+          }}
         />
         {markerPOI}
-
       </MapView>
 
+      <View style={{ margin: -10 }}>
+        <CheckBox
+          title="Credit/ Debit Card"
+          checked={credit}
+          onPress={() => setCredit(!credit)}
+        />
+        <CheckBox
+          title="Paypal"
+          checked={paypal}
+          onPress={() => setPaypal(!paypal)}
+        />
+        <CheckBox
+          title="Gpay :  Credit/ Debit Card"
+          checked={gpay}
+          onPress={() => setGpay(!gpay)}
+        />
+      </View>
 
-            <View style={{margin:-10}}>
-            
-              <CheckBox 
-              title='Credit/ Debit Card'
-              checked={credit}
-              onPress={() => setCredit(!credit)}/>
-              <CheckBox 
-              title='Paypal'
-              checked={paypal}
-              onPress={() => setPaypal(!paypal)}/>
-              <CheckBox 
-              title='Gpay :  Credit/ Debit Card'
-              checked={gpay}
-              onPress={() => setGpay(!gpay)}/>
-        
-        
-             <Button
-                title="Valider ma commande"
-                buttonStyle={{ backgroundColor: '#53B175', borderRadius: '10px' }}
-                containerStyle={{
-                  height: 40,
-                  marginHorizontal: 50,
-                  marginVertical: 10,
-                  alignItems: 'center', 
-                  
-                  
-                }}
-                titleStyle={{
-                  color: 'white',
-                  
-                }}
-              />
-        
-        </View>
+      <Button
+        title="Valider ma commande"
+        buttonStyle={{
+          backgroundColor: "#53B175",
+          borderRadius: 10,
+          margin: 30,
+          marginVertical: 10,
+        }}
+        onPress={() => props.navigation.navigate("Error")}
+      />
     </View>
   );
 }
